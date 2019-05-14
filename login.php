0ec3f463736e7cc436c846c515bb23bd06f1982e
leleors07 <leozinhocesam@hotmail.com>
2019-05-14 01:43:21 -0300

    tela de login, tela princial, e tela de cadastro de fornecedor responsilvel

--- login.php
+++ login.php
@@ -12,47 +12,36 @@ if(!$conexao){
 ?>
 <!DOCTYPE html>
 <html>
-    
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SISTEMA DE LOGIN</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-    <link rel="stylesheet" href="css/bulma.min.css" />
-    <link rel="stylesheet" type="text/css" href="css/login.css">
-</head>
+    <head>
+        <meta name="charset" content="utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+         <title>Cad_Fornecedores</title>
+		 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		 <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+         <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+                
+               <div class="container">
+               <div class="page-header">
+               <h1>Login</h1>
+               </div>
+               <div class="row">
+               <div class="col-sm-8">
+               <h3>Logar</h3>
+               <form>
 
-<body>
-    <section class="hero is-success is-fullheight">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Sistema de Login</h3>
-                    <div class="notification is-danger">
-                      <p>ERRO: Usuário ou senha inválidos.</p>
-                    </div>
-                    <div class="box">
-                        <form action="login.php" method="POST">
-                            <div class="field">
-                                <div class="control">
-                                    <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
-                                </div>
-                            </div>
+                  <div class="form-group">
+                  <input type="email" class="form-control" id="email" placeholder="Digite seu e-mail"
+                  </div>
+                  <div class="form-group">
+                  <h3>Senha</h3>
 
-                            <div class="field">
-                                <div class="control">
-                                    <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
-                                </div>
-                            </div>
-                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Entrar</button>
-
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</body>
-
-</html>
+                  <div class="form-group">
+                  <input type="password" class="form-control" id="cnpj" placeholder="Digite sua senha">
+                  </div>
+              
+                  <button type="submit" class="btn btn-primary">Logar</button>
+              </form>
+              <div class="form-group mb-3">
+           
---
+++ telaprincipal.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="charset" content="utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+         <title>Cad_Fornecedores</title>
+		 <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		 <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+         <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    </head>
+    <body>
+                
+               <nav class="navbar navbar-fixed-top navbar-default">
+               <div class="container">
+               <!-- header -->
+            <div class="navbar-header">
+            <a href="telaprincipal.php" class="navbar-brand">
+            Controle de Estoque
+            </a>
+        </div>
+        <div class="navbar-header">
+
+        <!-- botao toggle -->
+        <button type="button" class="navbar-toggle collapsed"
+                tada-toggle="collapse" data-target="#barra-navegacao">
+        <span class="sr-only">alternar navegação</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+     </button>
+     <a href="index.php" class="navbar-brand">
+     </div>
+
+        <!-- navbar -->
+    <div class="collapse navbar-collapse" id="barra-navegacao">
+    <ul class="nav navbar-nav navbar-right">
+    <li><a href="">Entrar</a><li>
+    <li><a href="">Cadastro de Produto</a><li>
+    <li><a href="">Cadastro de Fornecedor</a><li>
+    <li><a href="">Compra</a><li>
+    </ul>
+    </div>
